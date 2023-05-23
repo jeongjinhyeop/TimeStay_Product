f6a09a88bb8e0b3141d8db98ebcb0c75c3470d18
JeongJinHyeop <jhh9619@gmail.com>
2023-05-24 07:22:40 +0900

    init spring project

---
+++ src/main/java/TimeStay/TimeStay_Product/TimeStayProductApplication.java
@@ -0,0 +1,13 @@
+package TimeStay.TimeStay_Product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeStayProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeStayProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TimeStay/TimeStay_Product/TimeStayProductApplicationTests.java
@@ -0,0 +1,13 @@
+package TimeStay.TimeStay_Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeStayProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
